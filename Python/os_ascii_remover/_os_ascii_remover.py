a3a1a96e98519563a231da8ca7d86b4abcf10dca
rmlebioda <lebioda.rafal@gmail.com>
2020-03-14 23:07:35 +0100

    New python script: ASCII remover from paths

---
+++ Python/os_ascii_remover/_os_ascii_remover.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+import os
+import sys
+
+def _replace_non_ascii(source, replace_with=' ') -> str:
+    res = ''
+    for _character in source:
+        if _character.isascii():
+            res += _character
+        else:
+            res += replace_with
+    return res
+
+def _rename_non_ascii_by_path(_root_path, _file, _type: str = ''):
+    _nu_file = _replace_non_ascii(_file)
+    if _file != _nu_file:
+        print('Renaming {} from to:'.format(_type))
+        print(_root_path + '\\' + _file)
+        print('\t{}'.format(_root_path + '\\' + _replace_non_ascii(_file)))
+        os.rename(_root_path + '\\' + _file, _root_path + '\\' + _replace_non_ascii(_file))
+
+
+def remove_ascii_from_paths(paths: List[str]) -> None:
+    for path in paths:
+        if not os.path.isdir(path):
+            print('[E](Action: Skipping) Provided path is not a dir: "{}"'.format(path))
+            continue
+        for (root, dirs, files) in os.walk(path):
+            for _file in files:
+                _rename_non_ascii_by_path(_file, root, 'file')
+        for (root, dirs, files) in os.walk(path):
+            for _dir in dirs:
+                _rename_non_ascii_by_path(_dir, root, 'dir')
---
+++ Python/os_ascii_remover/os_ascii_remover.py
@@ -0,0 +1,18 @@
+﻿# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+MIN_PYTHON_VERSION = (3, 7)
+
+if sys.version_info < MIN_PYTHON_VERSION:
+    print("This script '{}' requires Python version 3.7 or newer".format(__file__))
+    sys.exit(1)
+
+from _os_ascii_remover import remove_ascii_from_paths
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('You need to provide path(s) to change as program arguments')
+        exit(1)
+    remove_ascii_from_paths(sys.argv[1:])
