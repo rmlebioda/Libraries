f898c1974724c0a5effe03cc1d9607ab5862573d
rmlebioda <lebioda.rafal@gmail.com>
2020-08-16 23:26:08 +0200

    new features

--- Python/youtube_dl_ext/YoutubeDL.py
+++ Python/youtube_dl_ext/YoutubeDL.py
@@ -12,11 +12,8 @@ class YoutubeDL(youtube_dl.YoutubeDL):
 			return res['entries']
 		except Exception:
 			raise NotImplementedError('Failed to parse yt response due to lack of proper implementation of received response from method "YoutubeDL.extract_info"')
-
-
-
-if __name__ == '__main__':
-	def _extract_titles(from_playlist: playlist) -> List[str]:
+    
+	def _extract_titles(self, playlist) -> List[str]:
 		res = []
 		for video in playlist:
 			try:
@@ -30,13 +27,18 @@ if __name__ == '__main__':
 				except Exception as e:
 					raise ValueError('Failed to parse response, no element "title" in dictionary playlist.entries, failed to print details: ' + str(e))
 		return res
+    
+	def get_playlist_titles(self, url: str, verbose: bool = True):
+		_playlist = self.get_playlist(url, verbose)
+		return self._extract_titles(_playlist)
 
+
+
+if __name__ == '__main__':
 	import sys
 	if len(sys.argv) > 1:
 		link = sys.argv[1]
 	else:
 		link = 'https://www.youtube.com/watch?v=4Uc9IhBSW6k&list=PLy6N_9yB8Qwy6LL0J7zLUyW8XNX-BPeDl'
 	print(f"Running playlist test, download playlist in video '{link}")
-	ydl = YoutubeDL()
-	playlist = ydl.get_playlist(link)
-	print('\n'.join(_extract_titles(from_playlist=playlist)))
+	print('\n'.join(YoutubeDL().get_playlist_titles(link)))
